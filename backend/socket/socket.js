e6e98cbbc5db3c0c3c2f2a12d7698b35bc464764
omsingh-blip <singhom1911in@gmail.com>
2026-05-17 14:57:21 +0530

    Fixed socket.io CORS

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -4,29 +4,43 @@ let io;
 
 export const initSocket = (server) => {
 
-  io = new Server(server,{
-    cors:{
-      origin:[
+  io = new Server(server, {
+
+    cors: {
+      origin: [
         "http://localhost:5173",
         "https://pingo-ai.netlify.app"
       ],
-      methods:["GET","POST"],
-      credentials:true
+
+      methods: [
+        "GET",
+        "POST"
+      ],
+
+      credentials: true
     }
+
   });
 
-  io.on("connection",(socket)=>{
+  io.on("connection", (socket) => {
+
     console.log(
       "User connected:",
       socket.id
     );
 
-    socket.on("disconnect",()=>{
-      console.log(
-        "User disconnected:",
-        socket.id
-      );
-    });
+    socket.on(
+      "disconnect",
+      () => {
+
+        console.log(
+          "User disconnected:",
+          socket.id
+        );
+
+      }
+    );
+
   });
 
 };
